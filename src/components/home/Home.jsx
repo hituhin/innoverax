edc98d8c552557f7d018bdadbbd55b52b39e1b13
Sheikh Tuhin <hituhin09@gmail.com>
2024-02-19 13:26:27 +0600

    update

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './home.module.css'
+
+
+const Home = () => {
+  return (
+    <div className={styles.main}><h1>This is Home page</h1></div>
+  )
+}
+
+export default Home     
